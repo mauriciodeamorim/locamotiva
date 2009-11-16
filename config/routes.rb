842a2d02f7270b33c8308455590b1a83270b6a17
Mauricio de Amorim <amorim@timedev.com>
2009-11-15 23:17:52 -0200

    Separating race administrative resources.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 ActionController::Routing::Routes.draw do |map|
   map.resources :results
   map.resources :runners
-  map.resources :races
+  map.resources :races, :only => :index
 
+#  map.connect '', :controller => 'results'
   map.root :controller => "results"
+  map.connect 'admin/races/new', :controller => 'admin/races', :action => 'new'
   # The priority is based upon order of creation: first created -> highest priority.
 
   # Sample of regular route:
@@ -30,10 +32,10 @@ ActionController::Routing::Routes.draw do |map|
   #   end
 
   # Sample resource route within a namespace:
-  #   map.namespace :admin do |admin|
-  #     # Directs /admin/products/* to Admin::ProductsController (app/controllers/admin/products_controller.rb)
-  #     admin.resources :products
-  #   end
+     map.namespace :admin do |admin|
+       # Directs /admin/products/* to Admin::ProductsController (app/controllers/admin/products_controller.rb)
+       admin.resources :races
+     end
 
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
   # map.root :controller => "welcome"
