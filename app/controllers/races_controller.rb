7ee91aabd9181534a30fada4ff54e1e0ee793b51
Mauricio de Amorim <amorim@timedev.com>
2009-11-29 04:56:16 -0200

    Showing last results and races.

--- app/controllers/races_controller.rb
+++ app/controllers/races_controller.rb
@@ -1,6 +1,6 @@
 class RacesController < ApplicationController
 
   def index
-    @races = Race.all
+    @races = Race.find(:all, :order => 'date DESC')
   end
 end
--- app/models/result.rb
+++ app/models/result.rb
@@ -1,4 +1,13 @@
 class Result < ActiveRecord::Base
   belongs_to :race
   belongs_to :athlete
+ 
+  private
+  def self.last_race
+    Race.find(:last, :order => "date ASC")
+  end
+  
+  def self.by_race
+    self.last_race.results.find(:all, :order => "start_number ASC")
+  end
 end
