7bf51760cb9a1e770c49dcd388a8552871e30200
Mauricio de Amorim <amorim@timedev.com>
2009-12-03 00:19:19 -0200

    Creating load data and seed task.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #   
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Major.create(:name => 'Daley', :city => cities.first)
+
+namespace :db do
+   desc "Load seed fixtures (from spec/fixtures) into the current environment's database."
+   task :seed => :environment do
+      require 'active_record/fixtures'
+      Dir.glob(RAILS_ROOT + '/tmp/db/*.{yml,csv}').each do |file|
+         Fixtures.create_fixtures('tmp/db', File.basename(file, '.*'))
+         puts "Running data fixture #{file}"
+      end
+   end
+end
